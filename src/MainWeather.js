55faf241a33ee11a6192b140f81ba9d052985409
mariaapre <maria.e.apreza@gmail.com>
2020-10-16 22:38:52 -0500

    incorporated search engine

---
+++ src/MainWeather.js
@@ -0,0 +1,59 @@
+import React from "react";
+import CurrentDate from "./CurrentDate";
+
+export default function MainWeather(props) {
+  return (
+    <div className="MainWeather">
+      <ul>
+        <li>
+          <CurrentDate date={props.data.date} />
+        </li>
+      </ul>
+      <h1> {props.data.city}</h1>
+      <div className="row">
+        <div className="col-6">
+          <ul>
+            <li>
+              Precipitation: <span></span>-- %
+            </li>
+            <li>
+              Humidity: <span>{props.data.humidity}</span>%
+            </li>
+            <li>
+              Wind: <span>{props.data.wind}</span> km/h
+            </li>
+          </ul>
+        </div>
+        <div className="col-6">
+          <img src="" alt="Clear" />
+          <strong className="main">{Math.round(props.data.temperature)}</strong>
+          <span className="units">
+            <a href="/" className="active">
+              °C
+            </a>
+            |<a href="/">°F</a>
+          </span>
+          <ul>
+            <li className="text-capitalize">{props.data.description}</li>
+            <li>
+              Sunrise: <span>{props.data.sunrise}</span>
+            </li>
+            <li>
+              Sunset: <span>{props.data.sunset}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <h4>Weekly forecast:</h4>
+      <div className="row weather-forecast"></div>
+      <div>
+        <small>
+          <a href="https://github.com/mariaapre/weather-react" target="/">
+            Open-source code{" "}
+          </a>
+          by Maria Apreza
+        </small>
+      </div>
+    </div>
+  );
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import MainWeather from "./MainWeather";
 import axios from "axios";
 
 import "./Weather.css";
@@ -6,11 +7,12 @@ import "./App.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
   function handleResponse(response) {
     setWeatherData({
       ready: true,
       city: response.data.name,
-      date: "Thursday 09:50",
+      date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
       sunrise: response.data.sys.sunrise,
@@ -19,13 +21,28 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
     });
   }
+  function search() {
+    const apiKey = "bd8ecef3a5464b3533486b9092216486";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search(); //another api call that updated the state
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value); //updates the city entered
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
         <div className="container">
           <div className="weather-app-wrapper">
             <div className="weather-app">
-              <form className="mb-3">
+              <form className="mb-3" onSubmit={handleSubmit}>
                 <div className="row">
                   <div className="col-6">
                     <input
@@ -33,6 +50,8 @@ export default function Weather(props) {
                       placeholder="Enter city"
                       className="form-control"
                       autoComplete="off"
+                      autoFocus="on"
+                      onChange={handleCityChange}
                     />
                   </div>
                   <div className="col-3">
@@ -52,66 +71,14 @@ export default function Weather(props) {
                   </div>
                 </div>
               </form>
-              <ul>
-                <li>{weatherData.date}</li>
-              </ul>
-              <h1> {weatherData.city} </h1>
-              <div className="row">
-                <div className="col-6">
-                  <ul>
-                    <li>
-                      Precipitation: <span></span>-- %
-                    </li>
-                    <li>
-                      Humidity: <span>{weatherData.humidity}</span>%
-                    </li>
-                    <li>
-                      Wind: <span>{weatherData.wind}</span> km/h
-                    </li>
-                  </ul>
-                </div>
-                <div className="col-6">
-                  <img src="" alt="Clear" />
-                  <strong className="main">
-                    {Math.round(weatherData.temperature)}
-                  </strong>
-                  <span className="units">
-                    <a href="/" className="active">
-                      °C
-                    </a>
-                    |<a href="/">°F</a>
-                  </span>
-                  <ul>
-                    <li className="text-capitalize">
-                      {weatherData.description}
-                    </li>
-                    <li>
-                      Sunrise: <span>{weatherData.sunrise}</span>
-                    </li>
-                    <li>
-                      Sunset: <span>{weatherData.sunset}</span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <h4>Weekly forecast:</h4>
-              <div className="row weather-forecast"></div>
+              <MainWeather data={weatherData} />
             </div>
-            <small>
-              <a href="https://github.com/mariaapre/weather-react" target="/">
-                Open-source code{" "}
-              </a>
-              by Maria Apreza
-            </small>
           </div>
         </div>
       </div>
     );
   } else {
-    const apiKey = "bd8ecef3a5464b3533486b9092216486";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
+    return "Loading...";
   }
-
-  return "Loading...";
 }
